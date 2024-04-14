46c38ef53f69500aa26fd5be1027c5fd6cab72c0
이승호 <seungho5623@gmail.com>
2024-04-14 14:01:54 +0900

    fix: 예약안내 innerHTML 적용
    예약안내 innerHTML 적용

--- didden/components/tourDetail/leportsDetail.tsx
+++ didden/components/tourDetail/leportsDetail.tsx
@@ -10,7 +10,9 @@ export default function leportsDetail({ detail }: any) {
         <p>연락처 : {detail?.cellphone}</p>
         <p>애완동물동반가능정보 : {detail?.chkpetleports}</p>
         <p>개장시간 : {detail?.openperiod}</p>
-        <p>예약안내 : {detail?.reservation}</p>
+        <p>
+          예약안내 : <span dangerouslySetInnerHTML={{ __html: detail?.reservation }} />
+        </p>
         <p>이용시간 : {detail?.usetimeleports}</p>
       </div>
     </React.Fragment>
